6208364ed8fdac89dd3f365fe54e15772d5cb18d
Sergio Cano <canosergio90@gmail.com>
2022-03-04 03:28:00 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/main/java/io/musimap/catalog/data/store/adapter/out/persistence/TrackRepositoryImpl.java
+++ src/main/java/io/musimap/catalog/data/store/adapter/out/persistence/TrackRepositoryImpl.java
@@ -1,15 +1,10 @@
 package io.musimap.catalog.data.store.adapter.out.persistence;
 
 import io.musimap.catalog.data.store.adapter.out.persistence.entities.TrackDocument;
-import io.musimap.catalog.data.store.adapter.out.persistence.mappers.TrackMapper;
 import io.musimap.catalog.data.store.adapter.out.persistence.mongo.TrackMongoRepository;
 import io.musimap.catalog.data.store.domain.entities.Track;
 import io.musimap.catalog.data.store.port.out.TrackRepository;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Arrays;
-import java.util.Date;
-import java.util.UUID;
 
 import static io.musimap.catalog.data.store.adapter.out.persistence.mappers.TrackMapper.documentToDomain;
 import static io.musimap.catalog.data.store.adapter.out.persistence.mappers.TrackMapper.domainToDocument;
@@ -18,16 +13,14 @@ import static io.musimap.catalog.data.store.adapter.out.persistence.mappers.Trac
 public class TrackRepositoryImpl implements TrackRepository {
 
     private TrackMongoRepository repository;
-    private TrackMapper mapper;
 
-    public TrackRepositoryImpl(TrackMongoRepository repository, TrackMapper mapper) {
+    public TrackRepositoryImpl(TrackMongoRepository repository) {
         this.repository = repository;
-        this.mapper = mapper;
     }
 
     @Override
     public Track save(Track track) {
-        TrackDocument persistedDocument = domainToDocument(track);
+        TrackDocument persistedDocument = repository.save(domainToDocument(track));
         return documentToDomain(persistedDocument);
     }
 }
--- src/main/java/io/musimap/catalog/data/store/temporale/TracksService.java
+++ src/main/java/io/musimap/catalog/data/store/temporale/TracksService.java
@@ -1,8 +1,5 @@
 package io.musimap.catalog.data.store.temporale;
 
-import io.musimap.catalog.data.store.adapter.out.persistence.TrackMongoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -10,11 +7,11 @@ import java.util.logging.Logger;
 @Service
 public class TracksService {
 
-    @Autowired
-    private TrackMongoRepository tracksRepository;
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
+//    @Autowired
+//    private TrackMongoRepository tracksRepository;
+//
+//    @Autowired
+//    private MongoTemplate mongoTemplate;
 
     private static final Logger LOGGER = Logger.getLogger(TracksService.class.getSimpleName());
 
